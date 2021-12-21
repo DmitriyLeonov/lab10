805f76a1b659d55e79f49b10ec369fba4a17e0c7
DmitriyLeonov <dleonov701@gmail.com>
2021-12-21 19:42:12 +0300

    Добавьте файлы проекта.

---
+++ lab10/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Software<string> word = new Software<string>() {
+                Version = "2019",
+                LicenseType = "MIT",
+                LastUpdate = DateTime.Now.AddDays(-50)
+            };
+            Software<string> excel = new Software<string>()
+            {
+                Version = "2015",
+                LicenseType = "Apache",
+                LastUpdate = DateTime.Now.AddDays(-20)
+            };
+            Software<string> powerpoint = new Software<string>()
+            {
+                Version = "2021",
+                LicenseType = "MIT",
+                LastUpdate = DateTime.Now.AddDays(-5)
+            };
+            List<Software<string>> microsoft = new List<Software<string>>();
+            microsoft.Add(word);
+            microsoft.Add(excel);
+            microsoft.Add(powerpoint);
+            microsoft.Remove(excel);
+            Software<string> soft = microsoft.Find(item => item.Version == "2021");
+            soft.Print();
+            foreach(Software<string> item in microsoft)
+            {
+                Console.WriteLine(item.Version);
+            }
+
+            SortedList<int, string> keyValuePairs = new SortedList<int, string>();
+
+        }
+    }
+}
---
+++ lab10/Software.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    public class Software<T>: IList<T>
+    {
+        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public T Version { get; set; }
+        public string LicenseType { get; set; }
+        public DateTime LastUpdate { get; set; }
+
+        public void Update()
+        {
+            LastUpdate = DateTime.Now;
+        }
+        public void Print()
+        {
+            Console.WriteLine(this.Version.ToString() + "\n" + this.LicenseType + "\n" + this.LastUpdate);
+        }
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
